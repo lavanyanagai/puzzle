8ba8a20deab2a35f3b54f938d9538c5474156530
lavanyanagai <85651411+lavanyanagai@users.noreply.github.com>
2021-06-13 22:08:07 +0400

    Update easy.js

--- static/js/easy.js
+++ static/js/easy.js
@@ -181,13 +181,18 @@ function issolved()
  var sec = document.getElementById("seconds").innerHTML;
  var min= document.getElementById("minutes").innerHTML;  
  document.getElementById("win").style.display = "block";
-  myaudio.play(); 
+ playsound();
  document.getElementById("message1").innerText=" MOVES : "+move ;
  document.getElementById("message2").innerText="TIME : "+min+":"+sec;  
  var s=score(min,sec,move); 
  topscorer(s); 
  document.getElementById("message").innerText=" SCORE : "+s; 
 } 
+
+function playsound()
+{  document.getElementById("myaudio").play();}
+
+
 
 function score(min,sec,move)
 { min = min*60;
